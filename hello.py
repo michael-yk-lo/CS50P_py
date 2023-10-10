0d94364a563b9f76146ebc50144ed9118f44e7ea
git config --global user.name Michael <michael.lyk@outlook.com>
2023-10-10 23:25:32 +0800

    print function arguments

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 #hello.py
-x = input("What's your name? ")
-print("hello, ", x)
+name = input("What's your name? ")
+print("hello, ", name)
+print("hello, ")
+print(name)
+
+# print (*object, sep = " ", end = "\n")
+print("hello, ", name, sep='')
+print("hello, ", end="")
+print(name)
+
+# escape
+print ("hello, \"friend\"")
